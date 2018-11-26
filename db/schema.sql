12f35a80eeb57bdd11b087bd36c5de4fecb844d4
Vu Le <thuy.dtt1118@thuys-air.lan>
2018-11-25 22:28:24 -0600

    please work

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,10 @@
-### Schema
-
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int(10) NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	id int (10) AUTO_INCREMENT NOT NULL,
+	burger_name varchar (200) NOT NULL,
 	devoured BOOLEAN,
 	PRIMARY KEY (id)
 );
