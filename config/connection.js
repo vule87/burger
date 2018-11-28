d657b2864db08d1f145aabba63543ab0843507d2
Vu Le <thuy.dtt1118@thuys-air.lan>
2018-11-27 22:12:56 -0600

    adding to db

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection;
 
 // Make connection.
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB.URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
